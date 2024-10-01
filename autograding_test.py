c661ed6c8d848b26cee80bfe96378f533876c265
ryanpalatty <ryan.palatty@stu.ocsb.ca>
2024-10-01 22:19:14 +0000

    hello world update

--- autograding_test.py
+++ autograding_test.py
@@ -1,4 +1,4 @@
-import program;
+import program; 
 
 def test_program():
     assert program.myFunction() == "Hello World!"
--- program.py
+++ program.py
@@ -1,5 +1,7 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
-def myFunction():
+def myFunction("Hello World!"):
+  print(myFunction)
   # My Function
+
